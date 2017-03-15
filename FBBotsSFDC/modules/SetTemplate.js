3966bacdbfba49ff4685e38d8750b7fd82211a15
Nizam91 <nizam.sfdc@gmail.com>
2017-03-15 19:28:38 +0530

    Update SetTemplate.js

--- FBBotsSFDC/modules/SetTemplate.js
+++ FBBotsSFDC/modules/SetTemplate.js
@@ -37,8 +37,8 @@ var formatContact = function(contacts)
 	var OpptyName = contacts[0].get("Name");
 	var OpptyAmount = contacts[0].get("Amount");
 	
-	//responsetext = "Hello '"+ sName +"', Your Phone Number in our database is'"+ sMobilePhone +"' , Thanks for reaching us !,\r\n  Opprotunity Assinged You and details are: \r\n Oppotunity Name:'"+ OpptyName +"'\r\n Oppotunity Amount:'"+ OpptyAmount +"'";
-	responsetext = "Hello Tom, \r\n  Price Concession waiting for you: \r\n 1. PC: 80131077 : Best Buy | PC Margin:68.50";
+	responsetext = "Hello '"+ sName +"', Your Phone Number in our database is'"+ sMobilePhone +"' , Thanks for reaching us !,\r\n  Opprotunity Assinged You and details are: \r\n Oppotunity Name:'"+ OpptyName +"'\r\n Oppotunity Amount:'"+ OpptyAmount +"'";
+	//responsetext = "Hello Tom, \r\n  Price Concession waiting for you: \r\n 1. PC: 80131077 : Best Buy | PC Margin:68.50";
 	console.log(responsetext);
 
 	return responsetext;
